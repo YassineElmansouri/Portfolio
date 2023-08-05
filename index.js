71e6757174ba647b19cb6eae4077a317a9a1cbee
yassine <yassineelmansouri44@gmail.com>
2023-08-05 14:47:45 +0100

    added some animations

--- index.js
+++ index.js
@@ -45,3 +45,19 @@ links.forEach(function (link) {
         smoothScroll(target, duration);
     });
 });
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+const hiddenElement = document.querySelectorAll('.hidden')
+hiddenElement.forEach((el)=>observer.observe(el))
